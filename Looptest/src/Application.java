ac895487ab4671e224c3deac037a9c1640fc2fa8
Arvind <Arvind@192.168.0.122>
2019-11-19 22:03:39 -0800

    loop test

---
+++ Looptest/src/Application.java
@@ -0,0 +1,20 @@
+
+public class Application {
+	public static void main(String[] args) {// java virtual machine calls method
+
+		int i = 1;
+		do {
+
+			i++;
+			System.out.println(i);
+
+		} while (i < 10);
+
+		while (i < 10) {
+
+			System.out.println(i);
+
+		}
+
+	}
+}
