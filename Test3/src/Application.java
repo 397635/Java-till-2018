f2a9474ebb4eabab7387bb2267dc6eca5bf4757c
Arvind <Arvind@192.168.0.122>
2019-11-19 22:45:51 -0800

    test3

---
+++ Test3/src/Application.java
@@ -0,0 +1,15 @@
+
+public class Application {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Student s1 = new Student ();
+		s1.name = "";
+		
+		Student s2 = new Student ();
+		
+
+	}
+
+}
---
+++ Test3/src/Student.java
@@ -0,0 +1,10 @@
+
+public class Student {
+	String name;
+	
+	 Student () {
+		
+		System.out.println("Something new");
+	}
+
+}
