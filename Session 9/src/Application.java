01348c92d5040a03114dd459327c13b2c2c78c82
Arvind <Arvind@192.168.0.122>
2019-11-19 22:39:17 -0800

    Session 9

---
+++ Session 9/src/Application.java
@@ -0,0 +1,14 @@
+
+public class Application {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Person p1 = new Person ();
+		p1.name ="Gauri";
+		p1.age = 13;
+		p1.Persondetails();
+
+	}
+
+}
---
+++ Session 9/src/Person.java
@@ -0,0 +1,11 @@
+
+public class Person {
+	String name;
+	int age;
+
+	void Persondetails() {
+
+		System.out.println("Name is " + name);
+		System.out.println("\nAge is " + age);
+	}
+}
