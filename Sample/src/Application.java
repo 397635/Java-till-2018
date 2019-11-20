1393d5797f901b63b6c32cad815c2fe003460b3f
Arvind <Arvind@192.168.0.122>
2019-11-19 22:07:17 -0800

    sample

---
+++ Sample/src/Application.java
@@ -0,0 +1,15 @@
+
+public class Application {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Math m = new Math();
+		m.getTotal(3);
+		
+		
+	
+
+	}
+
+}
---
+++ Sample/src/Math.java
@@ -0,0 +1,22 @@
+
+public class Math {
+	
+	int number;
+
+	static void getTotal (int number) {
+		
+			
+			
+		
+		int sum = 0;
+		for(int i = 1 ;i<=number ;i = i++ ) {
+			sum = sum + i;
+		}
+		
+		System.out.println(sum);
+		
+		
+	}
+
+	
+}
