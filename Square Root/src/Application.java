dba3ed2a4d168cdfc17fec508c3a5be1bbe876ea
Arvind <Arvind@192.168.0.122>
2019-11-19 22:43:09 -0800

    square root method

---
+++ Square Root/src/Application.java
@@ -0,0 +1,11 @@
+import java.math.*;
+public class Application {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println(Math.sqrt(6));
+
+	}
+
+}
