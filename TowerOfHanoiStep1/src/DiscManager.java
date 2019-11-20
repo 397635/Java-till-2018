5196884e9a5ec8fa465cd2eaec4a10b5a41df9f0
Arvind <Arvind@192.168.0.122>
2019-11-19 22:47:24 -0800

    toh step 1

---
+++ TowerOfHanoiStep1/src/DiscManager.java
@@ -0,0 +1,22 @@
+
+public class DiscManager {
+	
+	Disc getSmall(){
+		System.out.println("returned small");
+		return null;
+		
+	}
+	Disc getMedium(){
+		System.out.println("returned medium");
+		return null;
+		
+	}
+	Disc getLarge(){
+		System.out.println("returned large");
+		return null;
+		
+	}
+	
+	
+
+}
---
+++ TowerOfHanoiStep1/src/Gamer.java
@@ -0,0 +1,13 @@
+
+public class Gamer {
+	
+	void create() {
+		System.out.println("creating screen");
+		
+	}
+	void move (int from, int to) {
+		System.out.println("moving " + from + "to " + to);
+	}
+	
+	
+}
