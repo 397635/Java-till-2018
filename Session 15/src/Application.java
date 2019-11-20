ff33cefa64ce4e34fb204c9a8ac7db10c582f52b
Arvind <Arvind@192.168.0.122>
2019-11-19 22:14:03 -0800

    session 15-person class

---
+++ Session 15/src/Application.java
@@ -0,0 +1,13 @@
+
+public class Application {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Person p1 = new Person ();
+		p1.getfullName("Gauri", "Arora");
+		
+	
+	}
+
+}
---
+++ Session 15/src/Person.java
@@ -0,0 +1,18 @@
+
+public class Person {
+	
+	
+	String getfullName(String firstName, String lastName) {
+		
+		String fullname = firstName + "_" + lastName;
+		
+		
+		
+		
+		
+		return fullname;
+	}
+		
+	}
+
+
