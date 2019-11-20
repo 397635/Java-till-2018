492ddf53a5377de5cd295865b6a3ea506986ceb4
Arvind <Arvind@192.168.0.122>
2019-11-19 22:46:40 -0800

    times table

---
+++ Times Table/src/Application.java
@@ -0,0 +1,15 @@
+
+public class Application {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Math m = new Math();
+		m.printTable(5);
+		
+		Math m2 = new Math();
+		m2.printTable(7);
+
+	}
+
+}
---
+++ Times Table/src/Math.java
@@ -0,0 +1,21 @@
+
+public class Math {
+	
+	static void printTable (int x) {
+		
+		int i;
+		int  a;
+		
+		for ( i=1; i<=10; i=i+1) {
+			
+			  a = i * x;
+		System.out.println(x + " * " +  i + " = " + a);
+		}
+	}
+}
+	
+	
+		
+		
+
+
